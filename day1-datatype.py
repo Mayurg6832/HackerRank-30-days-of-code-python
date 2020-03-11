ce889c7c0319b5f8293c5cb2079bdd0a481f98ec
Mayurg6832 <48622430+Mayurg6832@users.noreply.github.com>
2020-03-11 16:42:05 +0530

    Create day1-datatype.py

---
+++ day1-datatype.py
@@ -0,0 +1,13 @@
+
+# Declare second integer, double, and String variables.
+n=int(input())
+# Read and save an integer, double, and String to your variables.
+p=eval(input())
+# Print the sum of both integer variables on a new line.
+print(i+n)
+# Print the sum of the double variables on a new line.
+print(d+p)
+# Concatenate and print the String variables on a new line
+m=input()
+# The 's' variable above should be printed first.
+print(s+m)
