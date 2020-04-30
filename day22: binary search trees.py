985ee12135cab21696465b12d776947b03e475d6
Mayur Goyal <Mayurg6832.mg@gmail.com>
2020-04-30 23:24:43 +0530

    Create day22: binary search trees.py

---
+++ day22: binary search trees.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def getHeight(self,root):
+        if root == None:
+            return -1
+        # divide and conquer
+        depth_left = self.getHeight(root.left)
+        depth_right = self.getHeight(root.right)
+        depth = max(depth_left, depth_right) + 1
+        # return the final result
+        return depth
+        #Write your code here
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+height=myTree.getHeight(root)
+print(height)       
