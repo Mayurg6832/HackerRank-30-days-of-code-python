fcae6f54763d7d08ea24800250e27332006f5676
Mayur Goyal <Mayurg6832.mg@gmail.com>
2020-05-12 02:38:04 +0530

    Update day12-inheritance.py

--- day12-inheritance.py
+++ day12-inheritance.py
@@ -1,4 +1,11 @@
-
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
 
 class Student(Person):
     #   Class Constructor
@@ -36,3 +43,12 @@ class Student(Person):
             return "D"
         elif avrage in range(0,41):
             return "T"
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
